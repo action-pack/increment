67d8780cfcfeebd51ede1a8b0036f071769201c0
Kroese <kroese@users.noreply.github.com>
2023-04-30 03:39:59 +0200

    Update secret.js

--- secret.js
+++ secret.js
@@ -93,11 +93,13 @@ const boostrap = async () => {
 
     const response = await setSecret(data)
 
-    if(response.status != 304){
+    if(response.status != "304"){
       console.error(response.status)
     }
-    
-    console.log("Succesfully set secret..")
+    else
+    {
+      console.log("Succesfully set secret..")
+    }
 
   }catch (e) {
     console.error(e)
