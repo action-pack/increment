84b6d7bdd4efaf91975875647de9c190c923340f
Kroese <kroese@users.noreply.github.com>
2023-08-01 00:22:36 +0200

    Octokit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const github = require("@actions/github");
 const core = require("@actions/core");
+const github = require("@actions/github");
 
 const name = core.getInput("name");
 const token = core.getInput("token");
-
-const { Octokit } = require("@octokit/core");
-const octokit = new Octokit({ auth: token })
+const octokit = github.getOctokit(token);
 
 const context = github.context;
 const repoName = context.payload.repository.name;
