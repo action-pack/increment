8fe30b8576c7221c6ebf3261a08e511d1564d420
Kroese <kroese@users.noreply.github.com>
2025-09-13 15:46:52 +0200

    feat: Make visibility configurable

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const github = require("@actions/github");
 const token = core.getInput("token");
 const octokit = github.getOctokit(token);
 
-const visibility = "all";
 const name = input("name", "1");
 const amount = input("amount", "1");
+
+const visibility = input("visibility", "all");
 const push_to_org = (input("org", "") !== "");
 const owner = input("owner", github.context.payload.repository.owner.login);
 const repository = input("repository", github.context.payload.repository.name);
@@ -54,25 +55,19 @@ function increment(string, amount) {
 
 const createVariable = (data) => {
 
-  if (push_to_org) return createOrgVariable(data);
-
   let url = "POST " + path_();
   url += "/actions/variables";
 
-  return octokit.request(url, {
-    name: name,
-    value: data
-  });
-};
-
-const createOrgVariable = (data) => {
-
-  let url = "POST " + path_();
-  url += "/actions/variables";
+  if (push_to_org) {
+    return octokit.request(url, {
+      name: name,
+      visibility: visibility,
+      value: data
+    });
+  }
 
   return octokit.request(url, {
     name: name,
-    visibility: visibility,
     value: data
   });
 };
